564e7afc47b6b0d2d60bfe4d7e44f1d9f56b1ffa
aha <aha@scireum.de>
2013-12-07 15:20:04 +0100

    Updated build script

--- go/build.bash
+++ go/build.bash
@@ -2,37 +2,30 @@ export PATH=/opt/go/bin:$PATH
 source /opt/go/src/golang-crosscompile/crosscompile.bash
 
 cd ../go
+rm ../resources/assets/binaries/*
 
 go-linux-386 build sds.go
-rm ../resources/assets/binaries/*
 mv sds ../resources/assets/binaries/sds-linux-386
 
 go-linux-amd64 build sds.go
-rm ../resources/assets/binaries/*
-mv sds ../resources/assets/binaries/sds-linux-adm64
+mv sds.exe ../resources/assets/binaries/sds-linux-amd64.exe
 
 go-windows-386 build sds.go
-rm ../resources/assets/binaries/*
-mv sds ../resources/assets/binaries/sds-windows-386
+mv sds.exe ../resources/assets/binaries/sds-windows-386.exe
 
 go-windows-amd64 build sds.go
-rm ../resources/assets/binaries/*
-mv sds ../resources/assets/binaries/sds-windows-adm64
+mv sds ../resources/assets/binaries/sds-windows-amd64
 
 go-darwin-386 build sds.go
-rm ../resources/assets/binaries/*
 mv sds ../resources/assets/binaries/sds-darwin-386
 
 go-darwin-amd64 build sds.go
-rm ../resources/assets/binaries/*
-mv sds ../resources/assets/binaries/sds-darwin-adm64
+mv sds ../resources/assets/binaries/sds-darwin-amd64
 
 go-freebsd-386 build sds.go
-rm ../resources/assets/binaries/*
 mv sds ../resources/assets/binaries/sds-freebsd-386
 
 go-freebsd-amd64 build sds.go
-rm ../resources/assets/binaries/*
-mv sds ../resources/assets/binaries/sds-freebsd-adm64
+mv sds ../resources/assets/binaries/sds-freebsd-amd64
 
 cd ../build
