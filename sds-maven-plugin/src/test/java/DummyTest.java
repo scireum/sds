6d581787389e87e76421c229a295c120d13716bb
aha <aha@scireum.de>
2015-08-18 18:35:59 +0200

    Fixed tests for mojo

---
+++ sds-maven-plugin/src/test/java/DummyTest.java
@@ -0,0 +1,17 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+import org.junit.Test;
+
+public class DummyTest {
+
+    @Test
+    public void dummyTest() {
+
+    }
+}
